048953e5859cb23f080c5bd98ec5bfaafd14593e
jwentao <380793594@qq.com>
2016-10-28 10:11:44 +0800

    add 2048demo

--- index/2048/animation.js
+++ index/2048/animation.js
@@ -11,3 +11,12 @@ function showNumberWithAnimation(i,j,rendNum){
 		left:getPosLeft(i,j)
 	},50);
 }
+
+function showMoveAnimation( fromx , fromy , tox, toy ){
+
+    var numberCell = $('#number-cell-' + fromx + '-' + fromy );
+    numberCell.animate({
+        top:getPosTop( tox , toy ),
+        left:getPosLeft( tox , toy )
+    },200);
+}
